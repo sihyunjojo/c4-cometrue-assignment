681409ef4f619c0d2d5176a9fe4aa6011d960917
sihyunjojo <si4018@naver.com>
2025-06-20 00:32:18 +0900

    feat: 직접 정의한 Slice 반환 값 추가

---
+++ common/src/main/java/org/c4marathon/assignment/pagination/SliceResult.java
@@ -0,0 +1,30 @@
+package org.c4marathon.assignment.pagination;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SliceResult<T> {
+
+	private final List<T> contents;
+	private final boolean hasNext;
+
+	public static <T> SliceResult<T> of(List<T> contents, boolean hasNext) {
+		Objects.requireNonNull(contents, "contents must not be null");
+		return new SliceResult<>(contents, hasNext);
+	}
+
+	public static <T> SliceResult<T> empty() {
+		return new SliceResult<>(Collections.emptyList(), false);
+	}
+
+	public boolean hasNext() {
+		return hasNext;
+	}
+}
