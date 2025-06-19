bcdcb1c5cc9c334a0781d55d3984affe7918df76
sihyunjojo <si4018@naver.com>
2025-06-20 00:29:41 +0900

    refactor: service 단의 transaction 책임 제거

--- interface/src/main/java/org/c4marathon/assignment/usecase/mainaccount/MainAccountUsecase.java
+++ interface/src/main/java/org/c4marathon/assignment/usecase/mainaccount/MainAccountUsecase.java
@@ -2,6 +2,7 @@ package org.c4marathon.assignment.usecase.mainaccount;
 
 import org.c4marathon.assignment.domain.service.MainAccountService;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +12,7 @@ public class MainAccountUsecase {
 
 	private final MainAccountService mainAccountService;
 
+	@Transactional
 	public void resetAllDailyChargeAmount() {
 		mainAccountService.resetAllDailyChargeAmount();
 	}
--- member-domain/src/main/java/org/c4marathon/assignment/domain/service/MemberService.java
+++ member-domain/src/main/java/org/c4marathon/assignment/domain/service/MemberService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.c4marathon.assignment.domain.model.Member;
 import org.c4marathon.assignment.domain.repository.MemberRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +12,6 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 
-	@Transactional
 	public Member registerember(Member member) {
 		memberRepository.save(member);
 		return member;
