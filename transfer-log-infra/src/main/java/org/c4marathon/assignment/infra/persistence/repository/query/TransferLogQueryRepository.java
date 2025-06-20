cd4e694f27c43759c04be81b4025b53727bafc82
sihyunjojo <si4018@naver.com>
2025-06-20 10:21:12 +0900

    refactor: 반환값 jpa 객체로 변경

--- transfer-log-infra/src/main/java/org/c4marathon/assignment/infra/persistence/repository/query/TransferLogQueryRepository.java
+++ transfer-log-infra/src/main/java/org/c4marathon/assignment/infra/persistence/repository/query/TransferLogQueryRepository.java
@@ -2,17 +2,17 @@ package org.c4marathon.assignment.infra.persistence.repository.query;
 
 import java.time.LocalDateTime;
 
-import org.c4marathon.assignment.domain.model.TransferLog;
+import org.c4marathon.assignment.infra.persistence.entity.TransferLogJpaEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface TransferLogQueryRepository {
-	Slice<TransferLog> findAllByAccountNumberAndSendTimeAndIdAfterCursor(String accountNumber,
-		LocalDateTime cursorTime, Long cursorId, int size);
+    Slice<TransferLogJpaEntity> findAllByAccountNumberAndSendTimeAndIdAfterCursor(
+        String accountNumber, LocalDateTime cursorTime, Long cursorId, int size);
 
-	Slice<TransferLog> findAllByAccountNumberAndSendTimeAfterCursor(String accountNumber,
-		LocalDateTime cursorTime, int size);
+    Slice<TransferLogJpaEntity> findAllByAccountNumberAndSendTimeAfterCursor(
+        String accountNumber, LocalDateTime cursorTime, int size);
 
-	Page<TransferLog> findPageByAccountNumber(String accountNumber, Pageable pageable);
+    Page<TransferLogJpaEntity> findPageByAccountNumber(String accountNumber, Pageable pageable);
 }
