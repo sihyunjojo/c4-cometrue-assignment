f021d82962227455fd5ef1ac7d8ec9d3e4c847b7
sihyunjojo <si4018@naver.com>
2025-06-21 13:44:51 +0900

    docs: 예외 문구 변경

--- interface/src/main/java/org/c4marathon/assignment/api/transfer/dto/TransferRequestDto.java
+++ interface/src/main/java/org/c4marathon/assignment/api/transfer/dto/TransferRequestDto.java
@@ -6,7 +6,6 @@ import org.c4marathon.assignment.api.transfer.validation.DifferentAccounts;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 
 @Builder
@@ -23,7 +22,7 @@ public record TransferRequestDto(
 
 	@Schema(description = "송금 금액 (단위: 원)", example = "11111")
 	@NotNull(message = "송금 금액은 필수입니다.")
-	@Positive(message = "음수는 송금할 수 없습니다.")
+	@Positive(message = "송금 금액은 0보다 커야 합니다.")
 	Long amount
 ) implements AccountHolder {
 }
